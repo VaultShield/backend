18780d725d52863f0eae7b382540045595f002f7
Flussen <53578660+Flussen@users.noreply.github.com>
2024-04-17 22:34:38 +0200

    block: deactivate spring security for test

--- src/main/java/com/vaultshield/passwordmanager/config/SwaggerConfig.java
+++ src/main/java/com/vaultshield/passwordmanager/config/SwaggerConfig.java
@@ -1,21 +1,21 @@
-package com.vaultshield.passwordmanager.config;
+// package com.vaultshield.passwordmanager.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+// import io.swagger.v3.oas.models.Components;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.security.SecurityRequirement;
 
-@Configuration
-public class SwaggerConfig {                                    
-    @Bean
-    public OpenAPI api() { 
-        return new OpenAPI()
-        .info(new Info()
-            .title("test"))
-        .addSecurityItem(new SecurityRequirement())
-        .components(new Components());
-    }
-}
+// @Configuration
+// public class SwaggerConfig {                                    
+//     @Bean
+//     public OpenAPI api() { 
+//         return new OpenAPI()
+//         .info(new Info()
+//             .title("test"))
+//         .addSecurityItem(new SecurityRequirement())
+//         .components(new Components());
+//     }
+// }
--- src/main/java/com/vaultshield/passwordmanager/services/security/SecurityConfig.java
+++ src/main/java/com/vaultshield/passwordmanager/services/security/SecurityConfig.java
@@ -1,13 +1,9 @@
 package com.vaultshield.passwordmanager.services.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Configuration;
 
-@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -16,16 +12,16 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())  // CSRF !!
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/verifysession").permitAll() // to improve!!
-                .anyRequest().authenticated())
-            .httpBasic(httpBasic -> httpBasic.realmName("api"));
+    // @Bean
+    // SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .csrf(csrf -> csrf.disable())  // CSRF !!
+    //         .authorizeHttpRequests(auth -> auth
+    //             .requestMatchers("/api/auth/register" , "/api/auth/login", "/api/auth/verifysession").permitAll() // to improve!!
+    //             .anyRequest().authenticated())
+    //         .httpBasic(httpBasic -> httpBasic.realmName("api"));
 
-        return http.build();
-    }
+    //     return http.build();
+    // }
 
 }
