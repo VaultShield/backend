998b47d678ce14f3dbe29f1f0cd82e4dc9f69e21
Flussen <53578660+Flussen@users.noreply.github.com>
2024-04-18 20:04:35 +0200

    refactor: configs fails and spring security

--- src/main/java/com/vaultshield/passwordmanager/config/PasswordManagerProperties.java
+++ src/main/java/com/vaultshield/passwordmanager/config/PasswordManagerProperties.java
@@ -1,16 +1,13 @@
 package com.vaultshield.passwordmanager.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
 
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "password-manager")
-@Validated
 public class PasswordManagerProperties {
 
     private String jwtKey;
