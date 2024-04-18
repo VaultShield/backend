6b39e06946f0e1fbc243f67b017cc63e87f2aacb
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2024-04-18 20:27:50 +0200

    fix: add RequiredArgs anotation

--- src/main/java/com/vaultshield/passwordmanager/services/impl/LoginAndRegistrationServiceImpl.java
+++ src/main/java/com/vaultshield/passwordmanager/services/impl/LoginAndRegistrationServiceImpl.java
@@ -25,7 +25,7 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-
+@RequiredArgsConstructor
 public class LoginAndRegistrationServiceImpl implements LoginAndRegistrationService {
 
     final private PasswordManagerProperties properties;
--- src/main/java/com/vaultshield/passwordmanager/web/AuthController.java
+++ src/main/java/com/vaultshield/passwordmanager/web/AuthController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
     final private LoginAndRegistrationServiceImpl service;
