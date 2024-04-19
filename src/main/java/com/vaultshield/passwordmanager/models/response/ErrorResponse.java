1eb864a4458c8c00068eba4195a87fd5232c8631
Othamae <vero.gconesa@gmail.com>
2024-04-19 09:40:27 +0200

    feat: Added examples in ErrorResponse

--- src/main/java/com/vaultshield/passwordmanager/models/response/ErrorResponse.java
+++ src/main/java/com/vaultshield/passwordmanager/models/response/ErrorResponse.java
@@ -1,5 +1,8 @@
 package com.vaultshield.passwordmanager.models.response;
 
+import com.vaultshield.passwordmanager.documentation.ExampleValues;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponse {
+
+    @Schema(description = "Error message", example = ExampleValues.ERROR_500)
     private String message;
 
 }
