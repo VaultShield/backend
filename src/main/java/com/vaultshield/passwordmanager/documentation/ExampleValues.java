9facbd628c17b1bdeb3675658259a49fae75e09a
Othamae <vero.gconesa@gmail.com>
2024-04-19 09:38:53 +0200

    feat: Added ExampleValues file

---
+++ src/main/java/com/vaultshield/passwordmanager/documentation/ExampleValues.java
@@ -0,0 +1,22 @@
+package com.vaultshield.passwordmanager.documentation;
+
+public class ExampleValues {
+    public static final String EMAIL = "johndoe@email.com";
+
+    public static final String USERNAME = "johndoe";
+
+    public static final String PASSWORD = "password123";
+
+    public static final String ID = "e0ca1ae7-57be-4f49-8790-b40a78b2ba20";
+
+    public static final String ACTIVE = "true";
+
+    public static final String UPDATE_DATE = "2024-04-18T12:06:55.86491";
+
+    public static final String SOFT_DELETE_DATE = "null";
+
+    public static final String ERROR_NOT_FOUND = "User not found";
+
+    public static final String ERROR_500 = "Internal Server Error";
+
+}
