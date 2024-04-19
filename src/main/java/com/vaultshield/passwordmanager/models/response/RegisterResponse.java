619b7efed903be715f797022aa72385f9cdb058d
Othamae <vero.gconesa@gmail.com>
2024-04-19 16:06:37 +0200

    feat: Added anotation in Register Response/Request

--- src/main/java/com/vaultshield/passwordmanager/models/response/RegisterResponse.java
+++ src/main/java/com/vaultshield/passwordmanager/models/response/RegisterResponse.java
@@ -1,12 +1,23 @@
 package com.vaultshield.passwordmanager.models.response;
 
+import com.vaultshield.passwordmanager.documentation.ExampleValues;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Data
 @Getter
 @Setter
 public class RegisterResponse {
+
+    @Schema(description = "User ID", example = ExampleValues.ID)
     private String id;
+
+    @Schema(description = "Status code", example = ExampleValues.STATUS_200)
     private int status;
+
+    @Schema(description = "Login message", example = ExampleValues.LOGIN_MSG)
     private String message;
 }
