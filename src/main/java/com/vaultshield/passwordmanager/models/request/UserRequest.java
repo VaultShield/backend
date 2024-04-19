ebd576690933ae97a88fa23dca4073537f08dbd4
Othamae <vero.gconesa@gmail.com>
2024-04-19 09:39:39 +0200

    feat: Added examples in userRequest

--- src/main/java/com/vaultshield/passwordmanager/models/request/UserRequest.java
+++ src/main/java/com/vaultshield/passwordmanager/models/request/UserRequest.java
@@ -1,5 +1,8 @@
 package com.vaultshield.passwordmanager.models.request;
 
+import com.vaultshield.passwordmanager.documentation.ExampleValues;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,7 +10,9 @@ import lombok.Data;
 @AllArgsConstructor
 public class UserRequest {
 
+    @Schema(example = ExampleValues.USERNAME)
     private String username;
 
+    @Schema(example = ExampleValues.EMAIL)
     private String email;
 }
