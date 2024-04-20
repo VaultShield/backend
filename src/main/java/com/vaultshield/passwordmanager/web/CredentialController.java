b9c3a75a2dd9c338dabb6add2ec0d7080e82ced5
Othamae <vero.gconesa@gmail.com>
2024-04-20 19:06:07 +0200

    feat: Added Swagger and response to CredentialController

--- src/main/java/com/vaultshield/passwordmanager/web/CredentialController.java
+++ src/main/java/com/vaultshield/passwordmanager/web/CredentialController.java
@@ -1,15 +1,32 @@
 package com.vaultshield.passwordmanager.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vaultshield.passwordmanager.documentation.ErrorExamples.NotFoundErrorExample;
+import com.vaultshield.passwordmanager.mapper.CredentialMapper;
 import com.vaultshield.passwordmanager.models.dto.Credentials;
 import com.vaultshield.passwordmanager.models.request.ChangedCredentialsRequest;
 import com.vaultshield.passwordmanager.models.request.CommonIdRequest;
-import com.vaultshield.passwordmanager.models.request.CreateNewCredentialRequest;
-import com.vaultshield.passwordmanager.models.request.FindCredentialsRequest;
+import com.vaultshield.passwordmanager.models.request.CredentialRequest;
+import com.vaultshield.passwordmanager.models.response.CredentialResponse;
 import com.vaultshield.passwordmanager.services.impl.CredentialsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/api/credential")
@@ -17,9 +34,15 @@ public class CredentialController {
 
     @Autowired
     CredentialsServiceImpl service;
+
+    @Operation(summary = "Add a new credential")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = CredentialResponse.class))),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = NotFoundErrorExample.class)))
+    })
     @PostMapping("/insert-credentials")
-    private void getAllCredentials(@RequestBody CreateNewCredentialRequest request){
-        service.insertCredential(request);
+    private CredentialResponse getAllCredentials(@RequestBody CredentialRequest request) {
+        return CredentialMapper.toCredentialResponse(service.insertCredential(request));
     }
 
     @PatchMapping("/update-credential")
@@ -32,9 +55,14 @@ public class CredentialController {
         service.deleteCredential(request);
     }
 
-    @PostMapping("/find-all-credential")
-    private List<Credentials> createCredential(@RequestBody FindCredentialsRequest request){
-       return service.findAllCredentials(request);
+    @Operation(summary = "Find all user's credentials")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(array = @ArraySchema(schema = @Schema(implementation = CredentialResponse.class)))),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = NotFoundErrorExample.class)))
+    })
+    @GetMapping("/find-all-credential/{userId}")
+    private List<CredentialResponse> createCredential(@PathVariable String userId) {
+        return CredentialMapper.toCredentialResponseList(service.findAllCredentials(userId));
     }
 
     @PostMapping("/find-credential")
