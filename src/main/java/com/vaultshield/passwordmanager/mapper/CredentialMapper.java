646ae31e672be648277a82665d1ba010fb860b0b
Othamae <vero.gconesa@gmail.com>
2024-04-20 17:45:59 +0200

    feat: Added credentialMapper

---
+++ src/main/java/com/vaultshield/passwordmanager/mapper/CredentialMapper.java
@@ -0,0 +1,20 @@
+package com.vaultshield.passwordmanager.mapper;
+
+import com.vaultshield.passwordmanager.models.entities.CredentialsEntity;
+import com.vaultshield.passwordmanager.models.response.CredentialResponse;
+
+public class CredentialMapper {
+
+    public static CredentialResponse toCredentialResponse(CredentialsEntity credential) {
+        return new CredentialResponse(
+                credential.getId(),
+                credential.getCredentialTypeId(),
+                credential.getCreateDate(),
+                credential.getUpdateDate(),
+                credential.getDeletedDate(),
+                credential.getState(),
+                credential.getFavorite(),
+                credential.getGroupId(),
+                credential.getUser().getId());
+    }
+}
