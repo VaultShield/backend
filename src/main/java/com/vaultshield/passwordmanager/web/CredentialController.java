4895153ec7501976dcbdd90863ddf06046831dfd
Othamae <vero.gconesa@gmail.com>
2024-04-20 22:09:45 +0200

    feat: Documented con Swagger and refactored endpoints

--- src/main/java/com/vaultshield/passwordmanager/web/CredentialController.java
+++ src/main/java/com/vaultshield/passwordmanager/web/CredentialController.java
@@ -3,6 +3,7 @@ package com.vaultshield.passwordmanager.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vaultshield.passwordmanager.documentation.ErrorExamples.NotFoundCredentialExample;
 import com.vaultshield.passwordmanager.documentation.ErrorExamples.NotFoundErrorExample;
 import com.vaultshield.passwordmanager.mapper.CredentialMapper;
-import com.vaultshield.passwordmanager.models.request.CommonIdRequest;
 import com.vaultshield.passwordmanager.models.request.CredentialRequest;
 import com.vaultshield.passwordmanager.models.response.CredentialResponse;
 import com.vaultshield.passwordmanager.services.impl.CredentialsServiceImpl;
@@ -39,7 +40,7 @@ public class CredentialController {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = CredentialResponse.class))),
             @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = NotFoundErrorExample.class)))
     })
-    @PostMapping("/insert-credentials")
+    @PostMapping("/insert")
     private CredentialResponse getAllCredentials(@RequestBody CredentialRequest request) {
         return CredentialMapper.toCredentialResponse(service.insertCredential(request));
     }
@@ -49,15 +50,21 @@ public class CredentialController {
             @ApiResponse(responseCode = "200", description = "Credential updated successfully", content = @Content(schema = @Schema(implementation = CredentialResponse.class))),
             @ApiResponse(responseCode = "404", description = "Credential not found", content = @Content(schema = @Schema(implementation = NotFoundCredentialExample.class))),
     })
-    @PatchMapping("/update-credential/{id}")
+    @PatchMapping("/{id}")
     private CredentialResponse updateCredentials(@RequestBody CredentialRequest request,
             @PathVariable String id) {
         return CredentialMapper.toCredentialResponse(service.modifyCredential(request, id));
     }
 
-    @DeleteMapping("/delete-credential")
-    private void deleteCredential(@RequestBody  CommonIdRequest request){
-        service.deleteCredential(request);
+    @Operation(summary = "Delete an existing credential")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleted successfully", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Credential not found", content = @Content(schema = @Schema(implementation = NotFoundCredentialExample.class)))
+    })
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private void deleteCredential(@PathVariable String id) {
+        service.deleteCredential(id);
     }
 
     @Operation(summary = "Find all user's credentials")
@@ -65,7 +72,7 @@ public class CredentialController {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(array = @ArraySchema(schema = @Schema(implementation = CredentialResponse.class)))),
             @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = NotFoundErrorExample.class)))
     })
-    @GetMapping("/find-all-credential/{userId}")
+    @GetMapping("/find-all/{userId}")
     private List<CredentialResponse> createCredential(@PathVariable String userId) {
         return CredentialMapper.toCredentialResponseList(service.findAllCredentials(userId));
     }
@@ -75,7 +82,7 @@ public class CredentialController {
             @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = CredentialResponse.class))),
             @ApiResponse(responseCode = "404", description = "Credential not found", content = @Content(schema = @Schema(implementation = NotFoundCredentialExample.class)))
     })
-    @GetMapping("/find-credential/{id}")
+    @GetMapping("/{id}")
     private CredentialResponse findCredential(@PathVariable String id) {
         return CredentialMapper.toCredentialResponse(service.findOneCredential(id));
     }
