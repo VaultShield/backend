49c53b3d4f608fedd0ce77a7bbfc6aa03c243ee2
Othamae <vero.gconesa@gmail.com>
2024-04-20 19:04:42 +0200

    feat: Added Response to CredentialService

--- src/main/java/com/vaultshield/passwordmanager/services/CredentialsService.java
+++ src/main/java/com/vaultshield/passwordmanager/services/CredentialsService.java
@@ -1,21 +1,25 @@
 package com.vaultshield.passwordmanager.services;
 
-import com.vaultshield.passwordmanager.models.dto.Credentials;
-import com.vaultshield.passwordmanager.models.request.ChangedCredentialsRequest;
-import com.vaultshield.passwordmanager.models.request.CommonIdRequest;
-import com.vaultshield.passwordmanager.models.request.CreateNewCredentialRequest;
-import com.vaultshield.passwordmanager.models.request.FindCredentialsRequest;
-
 import java.util.List;
 
+import com.vaultshield.passwordmanager.exceptions.NotFoundException;
+import com.vaultshield.passwordmanager.exceptions.SaveException;
+import com.vaultshield.passwordmanager.models.dto.Credentials;
+import com.vaultshield.passwordmanager.models.entities.CredentialsEntity;
+import com.vaultshield.passwordmanager.models.request.ChangedCredentialsRequest;
+import com.vaultshield.passwordmanager.models.request.CommonIdRequest;
+import com.vaultshield.passwordmanager.models.request.CredentialRequest;
+
 public interface CredentialsService {
-    void insertCredential(CreateNewCredentialRequest request);
+
+    CredentialsEntity insertCredential(CredentialRequest request) throws SaveException;
+
     void modifyCredential(ChangedCredentialsRequest request);
 
     void deleteCredential(CommonIdRequest request);
 
-     List<Credentials> findAllCredentials(FindCredentialsRequest request);
+    List<CredentialsEntity> findAllCredentials(String userId) throws NotFoundException;
 
-     Credentials findOneCredential(CommonIdRequest request);
+    Credentials findOneCredential(CommonIdRequest request);
 
 }
