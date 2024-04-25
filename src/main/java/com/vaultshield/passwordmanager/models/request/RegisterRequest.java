e0a188d185bfdfb0b6bd096cb98198ba5242be32
Othamae <vero.gconesa@gmail.com>
2024-04-25 19:42:12 +0200

    fix: Fixed spelling

--- src/main/java/com/vaultshield/passwordmanager/models/request/RegisterRequest.java
+++ src/main/java/com/vaultshield/passwordmanager/models/request/RegisterRequest.java
@@ -1,7 +1,7 @@
 package com.vaultshield.passwordmanager.models.request;
 
 import com.vaultshield.passwordmanager.documentation.ExampleValues;
-import com.vaultshield.passwordmanager.utils.ValitationMessages;
+import com.vaultshield.passwordmanager.utils.ValidationMessages;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
@@ -15,16 +15,16 @@ import lombok.Setter;
 @Setter
 public class RegisterRequest {
 
-    @NotBlank(message = ValitationMessages.USERNAME_REQUIRED)
+    @NotBlank(message = ValidationMessages.USERNAME_REQUIRED)
     @Schema(example = ExampleValues.USERNAME)
     public String username;
 
-    @NotBlank(message = ValitationMessages.PASSWORD_REQUIRED)
+    @NotBlank(message = ValidationMessages.PASSWORD_REQUIRED)
     @Schema(example = ExampleValues.PASSWORD)
     public String password;
 
-    @NotBlank(message = ValitationMessages.EMAIL_REQUIRED)
-    @Email(message = ValitationMessages.WRONG_EMAIL)
+    @NotBlank(message = ValidationMessages.EMAIL_REQUIRED)
+    @Email(message = ValidationMessages.WRONG_EMAIL)
     @Schema(example = ExampleValues.EMAIL)
     public String email;
 }
