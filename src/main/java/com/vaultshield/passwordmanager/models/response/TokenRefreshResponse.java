94fede91cc3234edc217e455f7657a322978f9a3
Othamae <vero.gconesa@gmail.com>
2024-04-25 19:47:50 +0200

    feat: Added TokenRefreshResponse

---
+++ src/main/java/com/vaultshield/passwordmanager/models/response/TokenRefreshResponse.java
@@ -0,0 +1,27 @@
+package com.vaultshield.passwordmanager.models.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TokenRefreshResponse {
+
+    private String token;
+
+    private String refreshToken;
+
+    private String tokenType = "Bearer";
+
+    private String expiration;
+
+    public TokenRefreshResponse(String token, String refreshToken, String expiration) {
+        this.token = token;
+        this.refreshToken = refreshToken;
+        this.expiration = expiration;
+    }
+}
