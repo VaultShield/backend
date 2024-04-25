4ae0a1270d9ffece0d254e666ecf371361033059
Othamae <vero.gconesa@gmail.com>
2024-04-25 19:59:18 +0200

    refactor: Refactored AuthController

--- src/main/java/com/vaultshield/passwordmanager/web/AuthController.java
+++ src/main/java/com/vaultshield/passwordmanager/web/AuthController.java
@@ -1,35 +1,28 @@
 package com.vaultshield.passwordmanager.web;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vaultshield.passwordmanager.documentation.ErrorExamples.ErrorResponse409Example;
 import com.vaultshield.passwordmanager.documentation.ErrorExamples.UnauthorizedErrorExample;
-
-import com.vaultshield.passwordmanager.config.PasswordManagerProperties;
-
-import com.vaultshield.passwordmanager.models.request.ChangePasswordRequest;
 import com.vaultshield.passwordmanager.models.request.LoginRequest;
 import com.vaultshield.passwordmanager.models.request.RegisterRequest;
-import com.vaultshield.passwordmanager.models.response.ChangePasswordResponse;
 import com.vaultshield.passwordmanager.models.response.LoginResponse;
 import com.vaultshield.passwordmanager.models.response.RegisterResponse;
-import com.vaultshield.passwordmanager.models.response.VerifySessionResponse;
-import com.vaultshield.passwordmanager.services.impl.LoginAndRegistrationServiceImpl;
-import com.vaultshield.passwordmanager.services.utils.VerifySession;
+import com.vaultshield.passwordmanager.services.UserService;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -39,20 +32,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-    final private LoginAndRegistrationServiceImpl service;
-    final private PasswordManagerProperties properties;
+    @Autowired
+    private UserService userService;
 
     @Operation(summary = "User registration", description = "Register a new user")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "user was registered correctly", content = {
+            @ApiResponse(responseCode = "201", description = "User was registered correctly", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterResponse.class))
             }),
             @ApiResponse(responseCode = "409", description = "Bad Request", content = @Content(schema = @Schema(implementation = ErrorResponse409Example.class))),
     })
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
-        RegisterResponse response = service.registerUser(request);
-        return ResponseEntity.status(response.getStatus()).body(response);
+    public ResponseEntity<RegisterResponse> registerUser(
+            @Valid @RequestBody @Schema(implementation = RegisterRequest.class) RegisterRequest signUpRequest) {
+        return userService.registerUser(signUpRequest);
     }
 
     @Operation(summary = "User login", description = "User authentication")
@@ -63,30 +56,9 @@ public class AuthController {
             @ApiResponse(responseCode = "401", description = "unauthorized", content = @Content(schema = @Schema(implementation = UnauthorizedErrorExample.class))),
     })
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request){
-        LoginResponse response = service.login(request);
-        return ResponseEntity.status(response.getStatus()).body(response);
+    public ResponseEntity<LoginResponse> loginUser(
+            @RequestBody @Validated @Schema(implementation = LoginRequest.class) LoginRequest loginRequest) {
+        return userService.loginUser((loginRequest));
     }
 
-    @Hidden
-    @GetMapping("/verifysession")
-    public ResponseEntity<?> verifySession(@RequestHeader("Authorization") String header){
-        VerifySession verifier = new VerifySession(properties);
-        VerifySessionResponse response = verifier.verifySession(header);
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
-    
-    // TEST!
-    @Hidden
-    @GetMapping("/dashboard")
-    public String dashboard(@RequestHeader String header) {
-        return new String();
-    }
-    
-    @Hidden
-    @PostMapping("/change-password")
-    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest request){
-        ChangePasswordResponse response = service.changePassword(request);
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
 }
