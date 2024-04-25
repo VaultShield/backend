4fc3ff3ca4d9b39141c0a4ec678f466783d458a2
Othamae <vero.gconesa@gmail.com>
2024-04-25 20:08:35 +0200

    feat: Created JWT classes

---
+++ src/main/java/com/vaultshield/passwordmanager/services/security/jwt/JwtAuthEntryPoint.java
@@ -0,0 +1,39 @@
+package com.vaultshield.passwordmanager.services.security.jwt;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaultshield.passwordmanager.models.response.ErrorResponse;
+import com.vaultshield.passwordmanager.utils.ErrorMessages;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+
+        log.error("Unauthorized error: {}", authException.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse(ErrorMessages.BAD_CREDENTIALS);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+
+    }
+}
