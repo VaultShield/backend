27024c9d405623bb39c669dc1a0f0eab2431ced3
Othamae <vero.gconesa@gmail.com>
2024-04-25 19:39:21 +0200

    feat: Added validation to RegisterRequest

--- src/main/java/com/vaultshield/passwordmanager/models/request/RegisterRequest.java
+++ src/main/java/com/vaultshield/passwordmanager/models/request/RegisterRequest.java
@@ -1,8 +1,11 @@
 package com.vaultshield.passwordmanager.models.request;
 
 import com.vaultshield.passwordmanager.documentation.ExampleValues;
+import com.vaultshield.passwordmanager.utils.ValitationMessages;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,12 +15,16 @@ import lombok.Setter;
 @Setter
 public class RegisterRequest {
 
+    @NotBlank(message = ValitationMessages.USERNAME_REQUIRED)
     @Schema(example = ExampleValues.USERNAME)
     public String username;
 
+    @NotBlank(message = ValitationMessages.PASSWORD_REQUIRED)
     @Schema(example = ExampleValues.PASSWORD)
     public String password;
 
+    @NotBlank(message = ValitationMessages.EMAIL_REQUIRED)
+    @Email(message = ValitationMessages.WRONG_EMAIL)
     @Schema(example = ExampleValues.EMAIL)
     public String email;
 }
