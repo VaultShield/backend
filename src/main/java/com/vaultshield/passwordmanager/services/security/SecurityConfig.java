fa462f1bee130cf5025652ddf1a3b62bd6a9f52e
Othamae <vero.gconesa@gmail.com>
2024-04-25 20:05:44 +0200

    feat: Added SecurityConfig to protect routes

--- src/main/java/com/vaultshield/passwordmanager/services/security/SecurityConfig.java
+++ src/main/java/com/vaultshield/passwordmanager/services/security/SecurityConfig.java
@@ -1,15 +1,48 @@
 package com.vaultshield.passwordmanager.services.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.vaultshield.passwordmanager.services.impl.UserDetailsServiceImpl;
+import com.vaultshield.passwordmanager.services.security.jwt.JwtAuthEntryPoint;
+import com.vaultshield.passwordmanager.services.security.jwt.JwtRequestFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthEntryPoint unauthorizedHandler;
+
+    @Bean
+    public JwtAuthEntryPoint unauthorizedHandler() {
+        return unauthorizedHandler;
+    }
+
+    @Bean
+    public JwtRequestFilter authenticationJwtTokenFilter() {
+        return new JwtRequestFilter();
+    }
+
+    @Bean
+    public UserDetailsServiceImpl userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
@@ -19,13 +52,17 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())  // CSRF !!
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/verifysession","/api/credential/**").permitAll() // to improve!!
-                        .requestMatchers("/api/user/**").permitAll()
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable()) // CSRF !!
+                .exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(httpBasic -> httpBasic.realmName("api"));
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
