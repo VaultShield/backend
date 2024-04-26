801c738bdb369fa171b7d11f1761960fb4fb5ab4
Othamae <vero.gconesa@gmail.com>
2024-04-26 11:37:30 +0200

    feat: Added Transactional to RefreshTokenRepository

--- src/main/java/com/vaultshield/passwordmanager/security/repository/RefreshTokenRepository.java
+++ src/main/java/com/vaultshield/passwordmanager/security/repository/RefreshTokenRepository.java
@@ -1,17 +1,20 @@
-package com.vaultshield.passwordmanager.repository;
+package com.vaultshield.passwordmanager.security.repository;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
 
-import com.vaultshield.passwordmanager.models.entities.RefreshTokenEntity;
 import com.vaultshield.passwordmanager.models.entities.UserEntity;
+import com.vaultshield.passwordmanager.security.model.RefreshTokenEntity;
 
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, String> {
 
     Optional<RefreshTokenEntity> findByToken(String token);
 
-    @Modifying
-    int deleteByUser(UserEntity user);
+    void deleteByUser(UserEntity user);
 }
