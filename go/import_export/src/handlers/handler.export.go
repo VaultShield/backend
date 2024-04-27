d5adbe4163d7d08e28b3cb4779efb59de26fbb3e
Flussen <53578660+Flussen@users.noreply.github.com>
2024-04-27 23:21:45 +0200

    refactor: changed response status, and support for only 1 status

--- go/import_export/src/handlers/handler.export.go
+++ go/import_export/src/handlers/handler.export.go
@@ -27,30 +27,47 @@ func ToExportHandler() fiber.Handler {
 			})
 		}
 
-		dataByte, err := services.ToJSONExport(rqst)
-		if err != nil {
-			if err == error_handler.Err_NOT_FOUND {
-				return c.Status(fiber.StatusNotFound).JSON(response.StandardHttpResponse{
-					Message: "this user does not have passwords",
-					Status:  fiber.StatusNotFound,
-					Data:    nil,
+		if rqst.CredentialsType == "password" {
+			var attachment string
+			var dataByte []byte
+			var err error
+
+			if rqst.FormatToExport == "json" {
+				dataByte, err = services.ToJSONExport(rqst)
+				if err != nil {
+					if err == error_handler.Err_NOT_FOUND {
+						return c.Status(fiber.StatusNotFound).JSON(response.StandardHttpResponse{
+							Message: "this user does not have passwords",
+							Status:  fiber.StatusNotFound,
+							Data:    nil,
+						})
+					}
+
+					return c.Status(fiber.StatusInternalServerError).JSON(response.StandardHttpResponse{
+						Message: "Error generating export",
+						Status:  fiber.StatusInternalServerError,
+						Data:    nil,
+					})
+				}
+			} else {
+				errorString := fmt.Sprintf("unimplemented export %s type, try with 'json'", rqst.FormatToExport)
+				return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+					"Message": errorString,
 				})
 			}
 
-			return c.Status(fiber.StatusInternalServerError).JSON(response.StandardHttpResponse{
-				Message: "Error generating export",
-				Status:  fiber.StatusInternalServerError,
-				Data:    nil,
-			})
+			attachment = fmt.Sprintf("attachment; filename=%s.json", rqst.Account)
+
+			c.Set("Content-Disposition", attachment)
+			c.Set("Content-Type", "application/json")
+
+			encodedData := base64.StdEncoding.EncodeToString(dataByte)
+
+			return c.Status(fiber.StatusCreated).JSON(encodedData)
 		}
-
-		attachment := fmt.Sprintf("attachment; filename=%s.json", rqst.Account)
-
-		c.Set("Content-Disposition", attachment)
-		c.Set("Content-Type", "application/json")
-
-		encodedData := base64.StdEncoding.EncodeToString(dataByte)
-
-		return c.Status(fiber.StatusCreated).JSON(encodedData)
+		errorString := fmt.Sprintf("unimplemented credetial %s type, try with 'password'", rqst.CredentialsType)
+		return c.Status(fiber.StatusNotImplemented).JSON(fiber.Map{
+			"Message": errorString,
+		})
 	}
 }
