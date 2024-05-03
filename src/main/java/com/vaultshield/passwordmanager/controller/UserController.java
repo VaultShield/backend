aeee17125adc18595dace997929e5c3462566e64
Flussen <53578660+Flussen@users.noreply.github.com>
2024-05-03 21:51:09 +0200

    feat: changepassword controller added and fix confusion in application.yml default

--- src/main/java/com/vaultshield/passwordmanager/controller/UserController.java
+++ src/main/java/com/vaultshield/passwordmanager/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,10 @@ import com.vaultshield.passwordmanager.documentation.ErrorExamples.BadCredential
 import com.vaultshield.passwordmanager.documentation.ErrorExamples.Error400EmailExistExample;
 import com.vaultshield.passwordmanager.documentation.ErrorExamples.Error400UsernameExistExample;
 import com.vaultshield.passwordmanager.documentation.ErrorExamples.NotFoundErrorExample;
+import com.vaultshield.passwordmanager.documentation.ErrorExamples.UnauthorizedErrorExample;
 import com.vaultshield.passwordmanager.mapper.UserMapper;
+import com.vaultshield.passwordmanager.models.request.ChangePasswordRequest;
+import com.vaultshield.passwordmanager.models.request.RecoverRequest;
 import com.vaultshield.passwordmanager.models.request.UserRequest;
 import com.vaultshield.passwordmanager.models.response.UserResponse;
 import com.vaultshield.passwordmanager.services.impl.UserServiceImpl;
@@ -29,6 +33,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @Tag(name = "User", description = "Update user information")
 @RestController
@@ -63,6 +68,20 @@ public class UserController {
         public UserResponse updateUser(@RequestBody UserRequest user,
                         @PathVariable String id) {
                 return UserMapper.toUserResponse(userService.changeUserData(user, id));
+        }
+
+        @Operation(summary = "Update User Password")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "User updated successfully", content = @Content(schema = @Schema(implementation = UserResponse.class))),
+                        @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = NotFoundErrorExample.class))),
+                        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = UnauthorizedErrorExample.class)))
+        })
+        @PatchMapping("/password/{id}")
+        public UserResponse changeUserPassword(
+                @Valid @RequestBody @Schema(implementation = ChangePasswordRequest.class)
+                        ChangePasswordRequest request,
+                        @PathVariable String id) {
+                return changeUserPassword(request, id);
         }
 
         @Operation(summary = "Delete an existing user")
