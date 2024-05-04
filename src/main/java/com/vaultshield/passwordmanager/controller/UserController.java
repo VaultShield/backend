ddc3529bb78354b341ca7a77db2906727b413e31
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2024-05-04 22:25:09 +0200

    fix: remove path mapping to evite CORS errors

--- src/main/java/com/vaultshield/passwordmanager/controller/UserController.java
+++ src/main/java/com/vaultshield/passwordmanager/controller/UserController.java
@@ -1,24 +1,6 @@
 package com.vaultshield.passwordmanager.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.vaultshield.passwordmanager.documentation.ErrorExamples.BadCredentialsExample;
-import com.vaultshield.passwordmanager.documentation.ErrorExamples.Error400EmailExistExample;
-import com.vaultshield.passwordmanager.documentation.ErrorExamples.Error400UsernameExistExample;
-import com.vaultshield.passwordmanager.documentation.ErrorExamples.NotFoundErrorExample;
-import com.vaultshield.passwordmanager.documentation.ErrorExamples.UnauthorizedErrorExample;
+import com.vaultshield.passwordmanager.documentation.ErrorExamples.*;
 import com.vaultshield.passwordmanager.mapper.UserMapper;
 import com.vaultshield.passwordmanager.models.request.ChangePasswordRequest;
 import com.vaultshield.passwordmanager.models.request.UserRequest;
@@ -26,7 +8,6 @@ import com.vaultshield.passwordmanager.models.response.ChangePasswordResponse;
 import com.vaultshield.passwordmanager.models.response.UserResponse;
 import com.vaultshield.passwordmanager.services.impl.UserServiceImpl;
 import com.vaultshield.passwordmanager.utils.ErrorMessages;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -35,6 +16,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User", description = "Update user information")
 @RestController
@@ -77,7 +62,7 @@ public class UserController {
                         @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = NotFoundErrorExample.class))),
                         @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = UnauthorizedErrorExample.class)))
         })
-        @PatchMapping("/password/{id}")
+        @PutMapping("/password/{id}")
         public ResponseEntity<ChangePasswordResponse> changeUserPassword(
                 @Valid @RequestBody @Schema(implementation = ChangePasswordRequest.class)
                         ChangePasswordRequest request,
